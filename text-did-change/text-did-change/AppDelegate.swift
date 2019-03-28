1a4925c4924022fd28591737f2311106665d74cb
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2019-03-27 23:44:14 -0400

    text did change example

---
+++ text-did-change/text-did-change/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  text-did-change
+//
+//  Created by Gavin on 3/27/19.
+//  Copyright © 2019 Gavin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
