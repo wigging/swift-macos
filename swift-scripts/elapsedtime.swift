4273a52f8f94f4fd857abfd1c4f1f6b4c2fb36bf
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-06-24 23:35:34 -0400

    Add more elapsed time examples

--- swift-scripts/elapsedtime.swift
+++ swift-scripts/elapsedtime.swift
@@ -2,28 +2,53 @@
 Below are two examples of measuring elapsed time in Swift. The sleep function
 is used to delay the code execution for 4 seconds. Consequently, the elapsed
 time for both examples is approximately 4 seconds.
+
+Build and run this script using the following terminal commands:
+swiftc elapsedtime.swift; ./elapsedtime
+
+The print output should look similar to the following:
+Elapsed time is 4 seconds
+Elapsed time is 4.005115985870361 seconds
+Elapsed time is 4.005129098892212 seconds
+Elapsed time is 4.003226656 seconds
 */
 
 import Foundation
 
-// Example 1
-// ---------------------------------------------------------------------------
+func exampleOne() {
+    let tic = DispatchTime.now().uptimeNanoseconds
+    sleep(4)
+    let toc = DispatchTime.now().uptimeNanoseconds
 
-let tic = DispatchTime.now().uptimeNanoseconds
+    print("Elapsed time is \((toc - tic) / 1_000_000_000) seconds")
+}
 
-sleep(4)
+func exampleTwo() {
+    let tic = CFAbsoluteTimeGetCurrent()
+    sleep(4)
+    let toc = CFAbsoluteTimeGetCurrent()
 
-let toc = DispatchTime.now().uptimeNanoseconds
+    print("Elapsed time is \(toc - tic) seconds")
+}
 
-print("Elapsed time is \((toc - tic) / 1_000_000_000) seconds")
+func exampleThree() {
+    let tic = Date.now
+    sleep(4)
+    let toc = tic.timeIntervalSinceNow.magnitude
 
-// Example 2
-// ---------------------------------------------------------------------------
+    print("Elapsed time is \(toc) seconds")
+}
 
-let tic2 = CFAbsoluteTimeGetCurrent()
+func exampleFour() {
+    let clock = ContinuousClock()
+    let result = clock.measure {
+        sleep(4)
+    }
 
-sleep(4)
+    print("Elapsed time is \(result)")
+}
 
-let toc2 = CFAbsoluteTimeGetCurrent()
-
-print("Elapsed time is \(toc2 - tic2) seconds")
+exampleOne()
+exampleTwo()
+exampleThree()
+exampleFour()
